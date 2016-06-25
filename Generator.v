9ef5eec44032dabc90932d64d6dc4b5638c53a8f
Kevin Wang <kevin_wang95@yahoo.com>
2016-06-25 14:39:35 +0800

    Fix Baud Rate Clock

--- Generator.v
+++ Generator.v
@@ -4,13 +4,13 @@ input reset,
 output reg bot_clk,
 output reg smp_clk
 );
-reg [8:0]   to_smp,ctr1;
-reg [12:0]  to_Baud,ctr2;
+reg [7:0]   to_smp,ctr1;
+reg [11:0]  to_Baud,ctr2;
 
 initial
 begin
-    to_smp=9'b1_0100_010;    ctr1=9'b0;
-    to_Baud=13'b1_0100_0101_100;    ctr2=13'b0;
+    to_smp=8'b10100011;    ctr1=8'b0;
+    to_Baud=12'b101000101100;    ctr2=12'b0;
     smp_clk=0;
     bot_clk=0;
 end
@@ -18,22 +18,22 @@ always @(posedge sys_clk or negedge reset)
 begin
     if(~reset)
     begin
-        ctr1=9'b0;
-        ctr2=13'b0;
+        ctr1=8'b0;
+        ctr2=12'b0;
     end
     else
     begin
     if(ctr1==to_smp)
             begin
                 smp_clk<=~smp_clk;
-                ctr1<=9'b0+1'b1;
+                ctr1<=8'b0+1'b1;
             end
         else
             ctr1<=ctr1+1'b1;
     if(ctr2==to_Baud)
             begin
                 bot_clk<=~bot_clk;
-                ctr2<=13'b0+1'b1;
+                ctr2<=12'b0+1'b1;
             end
         else
             ctr2<=ctr2+1'b1;
